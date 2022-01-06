f45c93b7ea1fea5014b484df05b104179204d492
Chloezhang1997 <80869740+Chloezhang1997@users.noreply.github.com>
2022-01-05 23:31:23 -0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -28,3 +28,30 @@ function App() {
 }
 
 export default App;
+
+<footer>
+          This project was coded by{" "}
+          <a
+            href="https://1l8tt.csb.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Man Zhang
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Chloezhang1997/weather-app-react-chloe-zhang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://competent-booth-867554.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
